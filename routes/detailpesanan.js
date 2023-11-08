476a25e44f2bee1d20f6f6b494f6f28a8a78e29f
marufnurmuhammad <marufnur27@gmail.com>
2023-11-08 08:19:38 +0700

    detail by id

--- routes/detailpesanan.js
+++ routes/detailpesanan.js
@@ -43,4 +43,58 @@ router.get('/tampil', function(req, res, next) {
     });
 })
 
+/* TAMPIL DATA BERDASARKAN id_pesanan */
+router.get('/tampil/:id_pesanan', function(req, res, next) {
+    const idPesanan = req.params.id_pesanan;
+
+    DetailPesanan.findAll({
+        where: { id_pesanan: idPesanan },
+        include: [{
+                model: Pesanan,
+                as: 'pesanan',
+                attributes: ['tanggal_pesan', 'status'],
+            },
+            {
+                model: Produk,
+                as: 'produk',
+                attributes: ['nama_produk'],
+            },
+        ],
+    }).then(data => {
+        const modifiedData = data.map(item => {
+            return {
+                id_pesanan: item.id_pesanan,
+                tanggal_pesan: item.pesanan.tanggal_pesan,
+                status: item.pesanan.status,
+                id_produk: item.id_produk,
+                nama_produk: item.produk.nama_produk,
+                subtotal: item.kuantitas * item.subtotal,
+            };
+        });
+
+        if (modifiedData.length > 0) {
+            res.json({
+                status: true,
+                pesan: "Data ditemukan",
+                data: modifiedData
+            });
+        } else {
+            res.json({
+                status: false,
+                pesan: "Data tidak ditemukan",
+                data: null
+            });
+        }
+    }).catch(err => {
+        res.json({
+            status: false,
+            pesan: "Gagal Tampil: " + err.message,
+            data: []
+        });
+    });
+});
+
+
+
+
 module.exports = router;
