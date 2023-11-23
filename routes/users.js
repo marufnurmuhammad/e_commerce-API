9341e82660a41c321152ba361c14ddc6e2d92521
marufnurmuhammad <marufnur27@gmail.com>
2023-11-23 16:55:14 +0700

    update user

--- routes/users.js
+++ routes/users.js
@@ -4,28 +4,63 @@ var router = express.Router();
 var Users = require('../models/Users')
 
 
+// router.post('/', function(req, res, next) {
+//     var email = req.body.email;
+//     var password = req.body.password;
+
+//     Users.findOne({
+//         where: { email: email, password: password }
+//     }).then(data => {
+
+//         if (data) {
+//             res.json({
+//                 status: true,
+//                 pesan: "Berhasil Login",
+//                 data: req.body
+//             });
+//         } else {
+//             res.json({
+//                 status: false,
+//                 pesan: "Nama atau Password Salah",
+
+//             });
+//         }
+//     })
+// })
+
 router.post('/', function(req, res, next) {
     var email = req.body.email;
     var password = req.body.password;
 
     Users.findOne({
         where: { email: email, password: password }
-    }).then(data => {
+    }).then(user => {
+        if (user) {
+            const userData = {
+                nama: user.nama_pengguna,
+                email: user.email,
+                alamat: user.alamat,
+                nomorhp: user.no_hp
+            };
 
-        if (data) {
             res.json({
                 status: true,
                 pesan: "Berhasil Login",
-                data: req.body
+                data: userData
             });
         } else {
             res.json({
                 status: false,
                 pesan: "Nama atau Password Salah",
-
             });
         }
-    })
-})
+    }).catch(error => {
+        res.json({
+            status: false,
+            pesan: "Terjadi kesalahan dalam mencari pengguna",
+            error: error.message
+        });
+    });
+});
 
 module.exports = router;
