942ee2227eb11c6c6521c14bb342d53dc26dcff8
marufnurmuhammad <marufnur27@gmail.com>
2024-01-03 13:31:44 +0700

    updated

--- routes/users.js
+++ routes/users.js
@@ -3,61 +3,84 @@ var router = express.Router();
 
 var Users = require('../models/Users');
 const { post } = require('.');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 
+router.post('/login', async function(req, res, next) {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
 
-router.post('/login', function(req, res, next) {
-    var email = req.body.email;
-    var password = req.body.password;
+        const user = await Users.findOne({
+            where: { email: email }
+        });
 
-    Users.findOne({
-        where: { email: email, password: password }
-    }).then(user => {
         if (user) {
-            const userData = {
-                nama: user.nama_pengguna,
-                email: user.email,
-                alamat: user.alamat,
-                nomorhp: user.no_hp
-            };
+            // Compare the provided password with the hashed password stored in the database
+            const passwordMatch = await bcrypt.compare(password, user.password);
 
-            res.json({
-                status: true,
-                pesan: "Berhasil Login",
-                data: userData
-            });
+            if (passwordMatch) {
+                const userData = {
+                    nama: user.nama_pengguna,
+                    email: user.email,
+                    alamat: user.alamat,
+                    nomorhp: user.no_hp
+                };
+
+                res.json({
+                    status: true,
+                    pesan: "Berhasil Login",
+                    data: userData
+                });
+            } else {
+                res.status(400).json({
+                    status: false,
+                    pesan: "Email atau Password Salah",
+                });
+            }
         } else {
             res.status(400).json({
                 status: false,
                 pesan: "Email atau Password Salah",
             });
         }
-    }).catch(error => {
+    } catch (error) {
         res.status(400).json({
             status: false,
-            pesan: "Terjadi kesalahan dalam mencari pengguna",
+            pesan: "Terjadi kesalahan dalam proses login",
             error: error.message
         });
-    });
+    }
 });
 
+router.post('/add', async function(req, res, next) {
+    try {
+        // Hash the password before saving it to the database
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
 
-router.post('/add', function(req, res, next) {
-    Users.create(req.body).then(data => {
-            res.json({
-                status: true,
-                pesan: "Berhasil Tambah User",
-                data: data
-            });
-        })
-        .catch(err => {
-            res.status(400).json({
-                status: false,
-                pesan: "Gagal Tambah: " + err.message,
-                data: []
-            });
+        // Replace the plain text password with the hashed one
+        const userObject = {
+            ...req.body,
+            password: hashedPassword
+        };
+
+        const data = await Users.create(userObject);
+
+        res.json({
+            status: true,
+            pesan: "Berhasil Tambah User",
+            data: data
         });
-})
+    } catch (err) {
+        res.status(400).json({
+            status: false,
+            pesan: "Gagal Tambah: " + err.message,
+            data: []
+        });
+    }
+});
+
 
 module.exports = router;
